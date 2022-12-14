66a09d132bcbec5ac3bfa82e30cfb4ae362c748d
Natalia Minenkova <nminiankova@gmail.com>
2022-12-14 17:55:13 +0300

    feat(components): add index for folder components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+import { MoviesList } from "./MoviesList/MoviesList";
+import { MovieCard } from "./MovieCard/MovieCard";
+import { Nav } from "./Nav/Nav";
+import { Search } from "./Search/Search";
