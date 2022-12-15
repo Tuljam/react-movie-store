b960377d1f1d928d38df48a73413b447c622b327
Natalia Minenkova <nminiankova@gmail.com>
2022-12-15 19:48:47 +0300

    fiat(icon): correct name by icon

--- src/components/NavLink/NavLink.tsx
+++ src/components/NavLink/NavLink.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Icon, NavLinkStyled, Text } from "./styles";
-import homeIcon from "../../assets/icons/homeIcons.svg";
-import trendIcon from "../../assets/icons/trendIcons.svg";
-import favoriteIcon from "../../assets/icons/favoriteIcons.svg";
-import settingsIcon from "../../assets/icons/settingIcons.svg";
+import homeIcon from "../../assets/icons/homepage-icons.svg";
+import trendIcon from "../../assets/icons/trendpage-icons.svg";
+import favoriteIcon from "../../assets/icons/favoritepage-icons.svg";
+import settingsIcon from "../../assets/icons/settingpage-icons.svg";
 
 export const NavLink = () => {
   return (
     <NavLinkStyled href="#">
-      <Icon src={homeIcon} alt="Home" />
+      <Icon src={homeIcon} alt="Home page icon" />
       <Text>Home</Text>
-      <Icon src={trendIcon} alt="Trend" />
+      <Icon src={trendIcon} alt="Trend page icon" />
       <Text>Trends</Text>
-      <Icon src={favoriteIcon} alt="Favorites" />
+      <Icon src={favoriteIcon} alt="Favorites page icon" />
       <Text>Favorites</Text>
-      <Icon src={settingsIcon} alt="Settings" />
+      <Icon src={settingsIcon} alt="Settings page icon" />
       <Text>Settings</Text>
     </NavLinkStyled>
   );
