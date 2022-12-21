36bcae16fdaa0948414ca965d8bffc21ddc710e4
Natalia Minenkova <nminiankova@gmail.com>
2022-12-21 21:02:36 +0300

    feat(src): add favoritespage&router

---
+++ src/pages/FavoritesPage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const FavoritesPage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem blanditiis accusamus fugiat,
+      itaque praesentium perferendis autem, commodi officia aspernatur exercitationem provident
+      similique voluptas quod porro sunt excepturi dolore minus inventore soluta quasi. Maxime
+      aspernatur sit possimus voluptatem culpa dolore exercitationem reprehenderit provident
+      corporis deleniti cupiditate tempore mollitia aut ipsa aliquam eligendi quis voluptates minus,
+      et nemo, qui impedit eum pariatur omnis. Magnam, culpa possimus similique accusantium amet rem
+      deleniti impedit alias reprehenderit excepturi temporibus pariatur facilis at maxime laborum
+      id dicta ipsa qui necessitatibus, rerum repudiandae eligendi. Dolor beatae expedita pariatur
+      minima rerum ipsum voluptate quis et, ratione voluptas corrupti omnis sed repellendus natus
+      necessitatibus eos dicta nemo amet eveniet quam cupiditate minus ipsa illo enim! Harum culpa
+      sed natus non laudantium dolore quasi reiciendis quia quis ea eaque aperiam dolor accusantium
+      incidunt eius molestias saepe iusto ut, sint nesciunt! Adipisci, beatae dignissimos. Beatae,
+      cupiditate
+    </div>
+  );
+};
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,12 @@
+import { FavoritesPage } from "pages/FavoritesPage";
 import { HomePage } from "pages/HomePage";
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path="/" element={<HomePage></HomePage>} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
     </Route>,
   ),
 );
