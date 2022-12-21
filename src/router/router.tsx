074551ff74831e077714946b829552cd02d8cda0
Natalia Minenkova <nminiankova@gmail.com>
2022-12-21 23:31:12 +0300

    feat(router): add index for router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,3 +1,4 @@
+import { MainTemplate } from "components/MainTemplate/MainTemplate";
 import { TrendsPage } from "pages";
 import { FavoritesPage } from "pages/FavoritesPage";
 import { HomePage } from "pages/HomePage";
@@ -6,8 +7,8 @@ import { ROUTE } from "./routes";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route>
-      <Route path={ROUTE.HOME} element={<HomePage />} />
+    <Route path={ROUTE.HOME} element={<MainTemplate />}>
+      <Route index path={ROUTE.HOME} element={<HomePage />} />
       <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
       <Route path={ROUTE.TRENDS} element={<TrendsPage />} />
     </Route>,
