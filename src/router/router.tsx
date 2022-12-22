c4b1e5796ba3906f4e5712d1df6a635223959782
Natalia Minenkova <nminiankova@gmail.com>
2022-12-22 15:46:56 +0300

    fix(router): correct code by router&index

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,5 @@
 import { MainTemplate } from "components";
-import { HomePage, FavoritesPage, TrendsPage } from "pages";
+import { HomePage, TrendsPage, FavoritesPage } from "pages";
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import { ROUTE } from "./routes";
 
@@ -7,8 +7,8 @@ export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={ROUTE.HOME} element={<MainTemplate />}>
       <Route index path={ROUTE.HOME} element={<HomePage />} />
-      <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
       <Route path={ROUTE.TRENDS} element={<TrendsPage />} />
+      <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
     </Route>,
   ),
 );
