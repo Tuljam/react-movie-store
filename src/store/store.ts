0252c34afd2cff2544f49e20ed27a6f1ad440afe
Natalia Minenkova <nminiankova@gmail.com>
2022-12-26 13:18:04 +0300

    feat(src): add useDispatch&useSelector to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import userReducer from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    // movies: movieReducer,
-    // newMovies: newMovieReduser,
-    // favoriteMovies:favoriteMovieReduser,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useTypedDispatch = () => useDispatch<AppDispatch>();
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
