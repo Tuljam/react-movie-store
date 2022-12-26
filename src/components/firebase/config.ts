450f9243d1ccca3fe4b073eefef23adb0d27a811
Natalia Minenkova <nminiankova@gmail.com>
2022-12-26 13:06:05 +0300

    init(src) init firebase

---
+++ src/components/firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCP6uIrYAHmbb-p6lUoKrpRDLWaAVvz5lE",
+  authDomain: "react-movie-store.firebaseapp.com",
+  projectId: "react-movie-store",
+  storageBucket: "react-movie-store.appspot.com",
+  messagingSenderId: "679330761372",
+  appId: "1:679330761372:web:d9cca380a7ea4dd8207b59",
+  measurementId: "G-Z4T8TP52NQ",
+};
