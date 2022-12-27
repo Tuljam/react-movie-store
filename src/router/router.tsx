afc5f5bb9dd09406a95a2e161789eec7c227eb3d
Natalia Minenkova <nminiankova@gmail.com>
2022-12-27 23:56:52 +0300

    fix(router): correct path for routehome

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -17,26 +17,23 @@ import { ROUTE } from "router";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route>
-      <Route element={<MainTemplate />}>
-        <Route path={ROUTE.HOME} element={<HomePage />} />
-        <Route path={ROUTE.TRENDS} element={<TrendsPage />} />
+    <Route path={ROUTE.HOME} element={<MainTemplate />}>
+      <Route index element={<HomePage />} />
+      <Route path={ROUTE.TRENDS} element={<TrendsPage />} />
 
-        {/* private route */}
-        <Route path={ROUTE.FAVORITES} element={<RequareAuth />}>
-          <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
-        </Route>
-        {/*  */}
-
-        <Route path={ROUTE.SETTINGS} element={<SettingsPage />} />
-        <Route path={ROUTE.MOVIE_CARD} element={<MovieCardPage />} />
-        <Route path={ROUTE.SEARCH} element={<SearchPage />} />
-        <Route path={ROUTE.PASSWORD} element={<UserPassWordPage />} />
-        <Route path={ROUTE.SIGN_IN} element={<SignInPage />} />
-        <Route path={ROUTE.SIGN_UP} element={<SignUpPage />} />
-        <Route path={ROUTE.DETAILS} element={<DetailsMoviePage />} />
+      {/* private route */}
+      <Route path={ROUTE.FAVORITES} element={<RequareAuth />}>
+        <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
       </Route>
-      ,
+      {/*  */}
+
+      <Route path={ROUTE.SETTINGS} element={<SettingsPage />} />
+      <Route path={ROUTE.MOVIE_CARD} element={<MovieCardPage />} />
+      <Route path={ROUTE.SEARCH} element={<SearchPage />} />
+      <Route path={ROUTE.PASSWORD} element={<UserPassWordPage />} />
+      <Route path={ROUTE.SIGN_IN} element={<SignInPage />} />
+      <Route path={ROUTE.SIGN_UP} element={<SignUpPage />} />
+      <Route path={ROUTE.DETAILS} element={<DetailsMoviePage />} />
     </Route>,
   ),
 );
