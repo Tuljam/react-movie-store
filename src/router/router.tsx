12495d5f4acb49c51391b28bb08708bde70843c1
Natalia Minenkova <nminiankova@gmail.com>
2022-12-28 07:10:23 +0300

    fix(rouret): add wrapper route for roters

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -17,23 +17,26 @@ import { ROUTE } from "router";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path={ROUTE.HOME} element={<MainTemplate />}>
-      <Route index element={<HomePage />} />
-      <Route path={ROUTE.TRENDS} element={<TrendsPage />} />
+    <Route>
+      <Route path={ROUTE.HOME} element={<MainTemplate />}>
+        <Route index element={<HomePage />} />
+        <Route path={ROUTE.TRENDS} element={<TrendsPage />} />
 
-      {/* private route */}
-      <Route path={ROUTE.FAVORITES} element={<RequareAuth />}>
-        <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
+        {/* private route */}
+        <Route path={ROUTE.FAVORITES} element={<RequareAuth />}>
+          <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
+        </Route>
+        {/*  */}
+
+        <Route path={ROUTE.SETTINGS} element={<SettingsPage />} />
+        <Route path={ROUTE.MOVIE_CARD} element={<MovieCardPage />} />
+        <Route path={ROUTE.SEARCH} element={<SearchPage />} />
+        <Route path={ROUTE.PASSWORD} element={<UserPassWordPage />} />
+        <Route path={ROUTE.SIGN_IN} element={<SignInPage />} />
+        <Route path={ROUTE.SIGN_UP} element={<SignUpPage />} />
+        <Route path={ROUTE.DETAILS} element={<DetailsMoviePage />} />
       </Route>
-      {/*  */}
-
-      <Route path={ROUTE.SETTINGS} element={<SettingsPage />} />
-      <Route path={ROUTE.MOVIE_CARD} element={<MovieCardPage />} />
-      <Route path={ROUTE.SEARCH} element={<SearchPage />} />
-      <Route path={ROUTE.PASSWORD} element={<UserPassWordPage />} />
-      <Route path={ROUTE.SIGN_IN} element={<SignInPage />} />
-      <Route path={ROUTE.SIGN_UP} element={<SignUpPage />} />
-      <Route path={ROUTE.DETAILS} element={<DetailsMoviePage />} />
+      ,
     </Route>,
   ),
 );
